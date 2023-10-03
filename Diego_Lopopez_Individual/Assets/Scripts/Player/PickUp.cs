4dca9d1bd4c3cdd42f3e58b0c4f0d2a5e8c70dd8
Diego López Navas <brutusverde1@gmail.com>
2023-10-04 00:03:29 +0200

    s
    sdf

--- Diego_Lopopez_Individual/Assets/Scripts/Player/PickUp.cs
+++ Diego_Lopopez_Individual/Assets/Scripts/Player/PickUp.cs
@@ -13,6 +13,7 @@ public class PickUp : NetworkBehaviour
     public float pickupRange;
     public float pickupForce;
     public float throwForce;
+    public NetworkVariable<bool> iHaveIt = new NetworkVariable<bool>();
 
 
     // Update is called once per frame
@@ -22,113 +23,80 @@ public class PickUp : NetworkBehaviour
 
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if(heldObject == null)
+            if(iHaveIt.Value == false)
             {
                 RaycastHit hit;
                 if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
                 {
-                    Debug.Log("El raycast se lanza");
                     PickupObjectServerRPC(transform.forward);
                 }
             }
-            else
+
+            else if (iHaveIt.Value == true)
             {
-                DropObject();
+                DropObjectServerRPC();
             }
-            
         }
 
         if (Input.GetKeyDown(KeyCode.G))
         {
-            ThrowObject();
+            Vector3 dir = transform.forward;
+            ThrowObjectServerRPC(dir);
         }
 
-        if (heldObject != null)
+        if (iHaveIt.Value == true)
         {
-            if (Vector3.Distance(heldObject.transform.position, holdArea.position) > 0.1f)
-            {
-                Vector3 moveDirection = (holdArea.transform.position - heldObject.transform.position);
-
-                MoveObjectServerRPC(moveDirection);
-            }
-
-
+            Vector3 holdA = holdArea.transform.position;
+            MoveObjectServerRPC(holdA);
         }
     }
 
-    void MoveObject()
+
+    [ServerRpc(RequireOwnership = false)]
+    private void MoveObjectServerRPC(Vector3 holdA)
     {
-        if(Vector3.Distance(heldObject.transform.position, holdArea.position) > 0.1f)
+        if (Vector3.Distance(heldObject.transform.position, holdA) > 0.1f)
         {
-            Vector3 moveDirection = (holdArea.transform.position - heldObject.transform.position);
+            Vector3 moveDirection = (holdA - heldObject.transform.position);
             heldObjRB.AddForce(moveDirection * pickupForce);
         }
     }
 
     [ServerRpc(RequireOwnership = false)]
-    private void MoveObjectServerRPC(Vector3 move)
-    {
-        if (Vector3.Distance(heldObject.transform.position, holdArea.position) > 0.1f)
-        {
-            heldObjRB.AddForce(move * pickupForce);
-            Debug.Log("El Objeto se mueve");
-            //Vector3 moveDirection = (holdArea.transform.position - heldObject.transform.position);
-            //heldObjRB.AddForce(moveDirection * pickupForce);
-        }
-    }
-
-
-
-
-    [ServerRpc(RequireOwnership = false)]
     private void PickupObjectServerRPC(Vector3 rotation)
     {
-        Debug.Log("El rpc se ejecuta");
         RaycastHit hit2;
         if (Physics.Raycast(transform.position, rotation, out hit2, pickupRange))
         {
-            Debug.Log("El raycast 2 se lanza");
             GameObject pickObj = hit2.transform.gameObject;
             if (pickObj.GetComponent<Rigidbody>())
             {
-                Debug.Log("Se supone que coge el objeto");
                 heldObjRB = pickObj.GetComponent<Rigidbody>();
                 heldObjRB.useGravity = false;
                 heldObjRB.drag = 10;
                 heldObjRB.constraints = RigidbodyConstraints.FreezeRotation;
                 heldObject = pickObj;
+                iHaveIt.Value = true;
             }
         }
     }
 
-
-
-    void PickupObject(GameObject pickObj)
-    {
-        if (pickObj.GetComponent<Rigidbody>())
-        {
-            heldObjRB = pickObj.GetComponent<Rigidbody>();
-            heldObjRB.useGravity = false;
-            heldObjRB.drag = 10;
-            heldObjRB.constraints = RigidbodyConstraints.FreezeRotation;
-            heldObject = pickObj;
-        }
-    }
-
-    void DropObject()
+    [ServerRpc(RequireOwnership = false)]
+    private void DropObjectServerRPC()
     {
         heldObjRB.useGravity = true;
         heldObjRB.drag = 1;
         heldObjRB.constraints = RigidbodyConstraints.None;
-        heldObject = null;
+        iHaveIt.Value = false;
     }
 
-    void ThrowObject()
+    [ServerRpc(RequireOwnership = false)]
+    private void ThrowObjectServerRPC(Vector3 direction)
     {
         heldObjRB.useGravity = true;
         heldObjRB.drag = 1;
         heldObjRB.constraints = RigidbodyConstraints.None;
-        heldObjRB.AddForce(throwForce * 100f * Time.deltaTime * transform.TransformDirection(Vector3.forward), ForceMode.Impulse);
-        heldObject = null;
+        heldObjRB.AddForce(throwForce * 1000f * Time.deltaTime * direction, ForceMode.Impulse);
+        iHaveIt.Value = false;
     }
 }
