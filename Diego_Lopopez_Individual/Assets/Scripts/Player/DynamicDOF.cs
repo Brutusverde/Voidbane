cac36befb37c98550e13c6f07b158de1951f349e
Diego López Navas <brutusverde1@gmail.com>
2023-10-08 00:52:25 +0200

    Order
    Orden en la escena, iluminación y prefabs

--- Diego_Lopopez_Individual/Assets/Scripts/Player/DynamicDOF.cs
+++ Diego_Lopopez_Individual/Assets/Scripts/Player/DynamicDOF.cs
@@ -6,6 +6,7 @@ using UnityEngine.Rendering.HighDefinition;
 
 public class DynamicDOF : MonoBehaviour
 {
+    public bool isEnabled;
     private DepthOfField testDoF;
     public Volume volume;
     public float hitDist;
@@ -16,25 +17,23 @@ public class DynamicDOF : MonoBehaviour
     void Start()
     {
         volume.profile.TryGet<DepthOfField>(out testDoF);
-
-        // Near blur
-        //testDoF.nearFocusStart.value = 0f;
-        //testDoF.nearFocusEnd.value = 0f;
-
-        //// Far blur
-        //testDoF.farFocusStart.value = 10f;
         testDoF.farFocusEnd.value = 10f;
     }
 
     private void Update()
     {
+        if (!isEnabled)
+        {
+            volume.gameObject.SetActive(false);
+        }
+
+        if (!isEnabled) return;
+        volume.gameObject.SetActive(true);
+
         RaycastHit hit;
         if (Physics.Raycast(cam.position, cam.forward, out hit, maxDist))
         {
             hitDist = Vector3.Distance(transform.position, hit.point);
-            //Debug.Log("hit" + hitDist);
-            //testDoF.nearFocusEnd.value = hitDist;
-            //testDoF.farFocusEnd.value = hitDist;
             testDoF.farFocusStart.value = Mathf.Lerp(testDoF.farFocusStart.value, hitDist, Time.deltaTime * focusSpeed);
 
             if(hitDist <= 10)
@@ -49,16 +48,6 @@ public class DynamicDOF : MonoBehaviour
                 testDoF.nearFocusStart.value = hitDist / 15f;
                 testDoF.nearFocusEnd.value = hitDist / 7f;
             }
-
-            
-
-            //testDoF.farFocusEnd.value = hitDist + 10f;
-            //testDoF.nearFocusStart.value = hitDist - 25f;
-            //testDoF.nearFocusEnd.value = hitDist - 15f;
-
-            //testDoF.farFocusStart.value = hitDist;
-            //testDoF.focusDistance.value = hitDist;
-            //Debug.Log(testDoF.farFocusEnd.value);
         }
     }
 }
