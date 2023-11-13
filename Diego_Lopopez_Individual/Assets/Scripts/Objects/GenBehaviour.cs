370b95a56ec1757f976729875d67eb706af336c1
Diego López Navas <brutusverde1@gmail.com>
2023-11-13 22:14:40 +0100

    Gen anim
    Gen with animation

--- Diego_Lopopez_Individual/Assets/Scripts/Objects/GenBehaviour.cs
+++ Diego_Lopopez_Individual/Assets/Scripts/Objects/GenBehaviour.cs
@@ -7,6 +7,17 @@ public class GenBehaviour : NetworkBehaviour
 {
     public LightControllerSO lightControllerSO;
     public Item fuelItem;
+
+    [Header("")]
+
+    public GameObject smoke;
+    public MeshRenderer lightMesh;
+    public Light lightComp;
+    public Material materialOff;
+    public Material materialOn;
+
+    public Animator animator;
+
     [Header("")]
 
     public NetworkVariable<bool> hasFuel = new NetworkVariable<bool>();
@@ -29,6 +40,17 @@ public class GenBehaviour : NetworkBehaviour
         if (lightController.CountDown.Value <= 0)
         {
             hasFuel.Value = false;
+            lightMesh.material = materialOff;
+            lightComp.color = Color.red;
+            animator.Play(null);
+            smoke.SetActive(false);
+        }
+        else
+        {
+            lightMesh.material = materialOn;
+            lightComp.color = Color.green;
+            animator.Play("GenOn");
+            smoke.SetActive(true);
         }
     }
 
