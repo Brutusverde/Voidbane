c2da7bcad1b3ca22849af40ec5357e2b996b3ee5
Diego López Navas <brutusverde1@gmail.com>
2023-12-18 20:37:22 +0100

    a

--- Diego_Lopopez_Individual/Assets/Scripts/Multi/CharacterSelect/CharacterSelectReady.cs
+++ Diego_Lopopez_Individual/Assets/Scripts/Multi/CharacterSelect/CharacterSelectReady.cs
@@ -55,7 +55,7 @@ public class CharacterSelectReady : NetworkBehaviour
             }
             else if(mazeMap.Value == true)
             {
-                Loader.LoadNetwork(Loader.Scene.Test3);
+                Loader.LoadNetwork(Loader.Scene.Test3_1);
             }
 
             else if (schoolMap.Value == false && mazeMap.Value == false)
